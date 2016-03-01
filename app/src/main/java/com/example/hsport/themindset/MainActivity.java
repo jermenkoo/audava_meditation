ce5f41bc4f69e8393e587e2ae1de21574b48b674
Lambros Zannettos <Lambros@Lambross-MacBook-Pro.local>
2016-03-01 12:50:07 +0000

    Started work on media player capabilities

--- app/src/main/java/com/example/hsport/themindset/MainActivity.java
+++ app/src/main/java/com/example/hsport/themindset/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.hsport.themindset;
 
+import android.media.Image;
 import android.media.MediaPlayer;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.MediaController;
+import android.widget.SeekBar;
+import android.widget.TextView;
 //import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
     Toolbar toolbar;
+
 
     MediaPlayer currentMeditation;
 
@@ -30,10 +38,42 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout.setDrawerListener(actionBarDrawerToggle);
 
         //For the media player capabilities
-        /*
-        MediaPlayer ourIntervention = MediaPlayer.create(this, R.raw.ocd);
-        ourIntervention.start();
-        */
+
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.ocd);
+        final SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+        final TextView txtTime = (TextView) findViewById(R.id.txt_time);
+
+        seekBar.setMax(mediaPlayer.getDuration());
+        txtTime.setText(Integer.toString(mediaPlayer.getDuration()));
+
+        final ImageButton playButton = (ImageButton) findViewById(R.id.btn_play_pause);
+        final ImageButton stopButton = (ImageButton) findViewById(R.id.btn_stop);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+                    playButton.setImageResource(R.drawable.btn_play);
+                }
+                else {
+                    mediaPlayer.start();
+                    playButton.setImageResource(R.drawable.btn_pause);
+                }
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mediaPlayer.isPlaying()) {
+                    mediaPlayer.stop();
+                    playButton.setImageResource(R.drawable.btn_play);
+                }
+            }
+        });
+
 
     }
 
@@ -42,6 +82,9 @@ public class MainActivity extends AppCompatActivity {
         super.onPostCreate(savedInstanceState);
 
         actionBarDrawerToggle.syncState();
-
     }
+
+
+
+
 }
