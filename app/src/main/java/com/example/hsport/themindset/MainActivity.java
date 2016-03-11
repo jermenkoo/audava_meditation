1d4da6bd33d9d1271d9b4135972e374b020210e9
Lambros Zannettos <Lambros@Lambross-MacBook-Pro.local>
2016-03-11 10:27:00 +0000

    added click listener for navigation menu

--- app/src/main/java/com/example/hsport/themindset/MainActivity.java
+++ app/src/main/java/com/example/hsport/themindset/MainActivity.java
@@ -1,18 +1,39 @@
 package com.example.hsport.themindset;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.BaseAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent startMeditationPlayer = new Intent(getApplicationContext(),MeditationPlayer.class);
-        startActivity(startMeditationPlayer);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_closed);
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
 
+        Intent startMeditationPlayer = new Intent(getApplicationContext(), MeditationPlayer.class);
+        startActivity(startMeditationPlayer);
     }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        actionBarDrawerToggle.syncState();
+    }
+
 }
