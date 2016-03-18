74ea8e3273b255b2e0f144c1cea370ef1a54ff23
Lambros Zannettos <Lambros@Lambross-MacBook-Pro.local>
2016-03-18 10:39:26 +0000

    fragment test

--- app/src/main/java/com/lambroszannettos/themindmanifesto/BaseActivity.java
+++ app/src/main/java/com/lambroszannettos/themindmanifesto/BaseActivity.java
@@ -1,11 +1,13 @@
 package com.lambroszannettos.themindmanifesto;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -28,6 +30,28 @@ public class BaseActivity extends AppCompatActivity {
         actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_closed);
         drawerLayout.setDrawerListener(actionBarDrawerToggle);
 
+        if(AppConstant.current_view == 0) {
+            AppConstant.current_view = R.layout.activity_main;
+        }
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.main_content, MainFragment.newInstance())
+                .commit();
+        drawerLayout.closeDrawers();
+//
+//        //This way a button (or other elements) can be added to a layout:
+//        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.main_area);
+//        Button button = new Button(this);
+//        button.setText("Test");
+//        linearLayout.addView(button);
+
+
+//        For each menu item selected, the appropriate activity is started, which will add the layout to the
+//        main_area LinearLayout in activity_main, and then the elements
+//
+
+
+
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem item) {
@@ -35,13 +59,36 @@ public class BaseActivity extends AppCompatActivity {
                     case R.id.menu_relationships:
                         MenuItem thisItem = item;
 //                        txtCurrentIntervention.setText("Finally!");
+                        AppConstant.current_view = R.layout.fragment_relationships;
+
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.main_content, MainFragment.newInstance())
+                                .commit();
                         drawerLayout.closeDrawers();
+                        break;
+
+                    case R.id.menu_home:
+
+                        AppConstant.current_view = R.layout.player_layout;
+
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.main_content, MainFragment.newInstance())
+                                .commit();
+                        drawerLayout.closeDrawers();
+
+                        Intent startPlayer = new Intent(getApplicationContext(), MeditationPlayer.class);
+                        startActivity(startPlayer);
+
+                        break;
+
                 }
                 return false;
             }
         });
 
     }
+
+
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
@@ -50,4 +97,8 @@ public class BaseActivity extends AppCompatActivity {
         actionBarDrawerToggle.syncState();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
 }
---
+++ app/src/main/java/com/lambroszannettos/themindmanifesto/MainFragment.java
@@ -0,0 +1,34 @@
+package com.lambroszannettos.themindmanifesto;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Lambros on 17/03/16.
+ */
+public class MainFragment extends Fragment {
+
+    View rootView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        rootView = inflater.inflate(AppConstant.current_view,container,false);
+
+        return rootView;
+    }
+
+    public static MainFragment newInstance() {
+        
+        Bundle args = new Bundle();
+        
+        MainFragment fragment = new MainFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+}
