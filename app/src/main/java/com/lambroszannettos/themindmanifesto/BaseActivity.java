557ef5563ad60074d3044165877c2d0b52ec1960
Lambros Zannettos <Lambros@Lambross-MBP.home>
2016-03-22 20:28:11 +0000

    optimised menu selection function, Layouts are not reloaded needlessly

--- app/src/main/java/com/lambroszannettos/themindmanifesto/BaseActivity.java
+++ app/src/main/java/com/lambroszannettos/themindmanifesto/BaseActivity.java
@@ -10,12 +10,15 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class BaseActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
     Toolbar toolbar;
+
+    static int currentLayoutId;
 
     public static final MediaPlayerSingleton mediaPlayerSingleton = MediaPlayerSingleton.getInstance();
     public static MediaPlayer mediaPlayer = mediaPlayerSingleton.getMediaPlayerInstance();
@@ -27,7 +30,6 @@ public class BaseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
-//        final FrameLayout contentFrameLayout = (FrameLayout) findViewById(R.id.content_frame);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -38,26 +40,33 @@ public class BaseActivity extends AppCompatActivity {
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem item) {
-                switch (item.getItemId()) {
 
-                    case R.id.menu_home:
-                        Intent loadPlayer = new Intent(getApplicationContext(), MeditationPlayer.class);
-                        startActivity(loadPlayer);
-                        drawerLayout.closeDrawers();
-                        break;
+                if (currentLayoutId != item.getItemId()) {
+                    switch (item.getItemId()) {
 
-                    case R.id.menu_relationships:
-                        Intent loadRelationships = new Intent(getApplicationContext(), ChooseMeditation.class);
-                        startActivity(loadRelationships);
-                        drawerLayout.closeDrawers();
-                        break;
+                        case R.id.menu_home:
+                            Intent loadPlayer = new Intent(getApplicationContext(), MeditationPlayer.class);
+                            startActivity(loadPlayer);
+                            drawerLayout.closeDrawers();
+                            break;
 
-                    case R.id.menu_about:
-                        Intent loadAbout = new Intent(getApplicationContext(), About.class);
-                        startActivity(loadAbout);
-                        drawerLayout.closeDrawers();
-                        break;
+                        case R.id.menu_relationships:
+                            Intent loadRelationships = new Intent(getApplicationContext(), ChooseMeditation.class);
+                            startActivity(loadRelationships);
+                            drawerLayout.closeDrawers();
+                            break;
 
+                        case R.id.menu_about:
+                            Intent loadAbout = new Intent(getApplicationContext(), About.class);
+                            startActivity(loadAbout);
+                            drawerLayout.closeDrawers();
+                            break;
+                    }
+
+                    currentLayoutId = item.getItemId();
+
+                } else {
+                    drawerLayout.closeDrawers();
                 }
                 return false;
             }
