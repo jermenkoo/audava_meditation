c38eda9d71450323afc13d08375c969d74e32abc
Lambros Zannettos <Lambros@Lambross-MBP.home>
2016-03-27 13:08:11 +0100

    reading/saving settings done

--- app/src/main/java/com/lambroszannettos/themindmanifesto/MyFunctions.java
+++ app/src/main/java/com/lambroszannettos/themindmanifesto/MyFunctions.java
@@ -1,22 +1,17 @@
 package com.lambroszannettos.themindmanifesto;
 
 import android.app.Application;
-import android.os.Environment;
-import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.widget.Toolbar;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 
-import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Lambros on 08/03/16.
- *
+ * <p/>
  * Functions in this class will be accessible from all other
  * activities, by typing MyFunctions.theFunctionName([...])
- *
  */
 public final class MyFunctions extends Application {
 
@@ -33,6 +28,32 @@ public final class MyFunctions extends Application {
                 TimeUnit.MILLISECONDS.toSeconds((long) milliseconds) -
                         TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.
                                 toMinutes((long) milliseconds)));
+    }
+
+
+    public void saveSetting(Context context, String key, String value) {
+        SharedPreferences sharedPref = context.getSharedPreferences("user_settings", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        editor.putString(key, value);
+
+        editor.commit();
+
+    }
+
+    public String readSetting(Context context,String key) {
+
+        SharedPreferences sharedPref;
+        String result = "";
+
+        try {
+            sharedPref = context.getSharedPreferences("user_settings", Context.MODE_PRIVATE);
+            result = sharedPref.getString(key, "");
+        } catch (NullPointerException e) {
+            Log.d("Cannot read settings", e.toString());
+        }
+
+        return result;
     }
 
     public static MyFunctions getUniqueInstance() {
