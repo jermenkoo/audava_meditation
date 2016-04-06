0d49095b15cca17fe67479e28d139e8402e57260
Lambros Zannettos <Lambros@Lambross-MBP.home>
2016-04-06 10:55:01 +0100

    several changes, mainly implemented filtered view of interventions

--- app/src/main/java/com/lambroszannettos/themindmanifesto/ChooseMeditation.java
+++ app/src/main/java/com/lambroszannettos/themindmanifesto/ChooseMeditation.java
@@ -1,19 +1,14 @@
 package com.lambroszannettos.themindmanifesto;
 
-import android.content.Context;
-import android.content.res.AssetFileDescriptor;
-import android.media.MediaMetadataRetriever;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
-import android.widget.Toast;
-
+import android.widget.TextView;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -24,6 +19,7 @@ public class ChooseMeditation extends BaseActivity {
 
     private ListView interventionList;
     private MyFunctions functions = MyFunctions.getUniqueInstance();
+    private TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,75 +31,43 @@ public class ChooseMeditation extends BaseActivity {
 
         //The ListView to be populated
         interventionList = (ListView) findViewById(R.id.med_list);
+        title = (TextView) findViewById(R.id.txt_choose_med_title);
 
         //Arraylist to hold all sets of titles and albums (i.e. categories
         //for audio files
         ArrayList<HashMap<String, String>> itemsList = new ArrayList<>();
 
-        //Get the list of all m4a files in Raw folder
-        ArrayList<File> files = functions.getAllFilesInAssetByExtension(this, AppConstant.INTERVENTION_FOLDER, ".m4a");
 
+        title.setText("Browsing: " + interventionCategory);
         //Add those files to the ListView
-        for (File f : files) {
-            String title = getAudioTitle(this, f, AppConstant.INTERVENTION_FOLDER);
-            String album = getAudioAlbum(this, f, AppConstant.INTERVENTION_FOLDER);
+        for (File f : allAudioFiles) {
+            String title = functions.getAudioTitle(this, f, AppConstant.INTERVENTION_FOLDER);
+            String album = functions.getAudioAlbum(this, f, AppConstant.INTERVENTION_FOLDER);
 
-            HashMap<String, String> itemsMap = new HashMap<>(2);
-
-            itemsMap.put("title", title);
-            itemsMap.put("album", album);
-            itemsList.add(itemsMap);
+            if (album.equals(interventionCategory) || interventionCategory.equals(AppConstant.BROWSE_ALL)) {
+                HashMap<String, String> itemsMap = new HashMap<>(2);
+                itemsMap.put("title", title);
+                itemsMap.put("album", album);
+                itemsList.add(itemsMap);
+            }
         }
 
         //Adapter for converting file items into View items to populate the ListView
-        SimpleAdapter adapter2 = new SimpleAdapter(this, itemsList,
+        SimpleAdapter adapter = new SimpleAdapter(this, itemsList,
                 android.R.layout.simple_list_item_2,
                 new String[]{"title", "album"},
                 new int[]{android.R.id.text1,
                         android.R.id.text2});
-        interventionList.setAdapter(adapter2);
+        interventionList.setAdapter(adapter);
 
         //Actions when item is selected
         interventionList.setOnItemClickListener(new android.widget.AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                Toast.makeText(ChooseMeditation.this, "" + position, Toast.LENGTH_SHORT).show();
+                //Call function which actually handles the switch of selected audio file
+                selectIntervention(position, true);
             }
         });
-    }
-
-
-    //Given an audio file, this returns the ID3 Title Field
-    private String getAudioTitle(Context context, File file, String path) {
-        AssetFileDescriptor afd;
-        MediaMetadataRetriever mmr = new MediaMetadataRetriever();
-
-        try {
-            afd = context.getAssets().openFd(path + file.getAbsolutePath());
-            mmr.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        String title =
-                mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
-        return title;
-    }
-
-    private String getAudioAlbum(Context context, File file, String path) {
-        AssetFileDescriptor afd;
-        MediaMetadataRetriever mmr = new MediaMetadataRetriever();
-
-        try {
-            afd = context.getAssets().openFd(path + file.getAbsolutePath());
-            mmr.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        String album =
-                mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM);
-        return album;
     }
 }
--- app/src/main/java/com/lambroszannettos/themindmanifesto/MyFunctions.java
+++ app/src/main/java/com/lambroszannettos/themindmanifesto/MyFunctions.java
@@ -3,6 +3,9 @@ package com.lambroszannettos.themindmanifesto;
 import android.app.Application;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.AssetFileDescriptor;
+import android.media.MediaMetadataRetriever;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import junit.framework.Assert;
@@ -18,7 +21,7 @@ import java.util.concurrent.TimeUnit;
  * Functions in this class will be accessible from all other
  * activities, by typing MyFunctions.theFunctionName([...])
  */
-public final class MyFunctions extends Application {
+public final class MyFunctions {
 
     private static MyFunctions uniqueInstance = new MyFunctions();
 
@@ -62,25 +65,58 @@ public final class MyFunctions extends Application {
     }
 
 
-    public ArrayList<File> getAllFilesInAssetByExtension(Context context, String subFolder, String extension) {
-        Assert.assertNotNull(context);
+//    public ArrayList<File> getAllFilesInAssetByExtension(Context context, String subFolder, String extension) {
+//        Assert.assertNotNull(context);
+//
+//        try {
+//            String[] files = context.getAssets().list(subFolder);
+//            ArrayList<File> filesWithExtension = new ArrayList<>();
+//
+//            for (String file : files) {
+//                if (file.endsWith(extension)) {
+//                    filesWithExtension.add(new File(file));
+//                }
+//            }
+//
+//            return filesWithExtension;
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+
+    //Given an audio file, this returns the ID3 Title Field
+    public String getAudioTitle(Context context, File file, String path) {
+        AssetFileDescriptor afd;
+        MediaMetadataRetriever mmr = new MediaMetadataRetriever();
 
         try {
-            String[] files = context.getAssets().list(subFolder);
-            ArrayList<File> filesWithExtension = new ArrayList<>();
-
-            for (String file : files) {
-                if (file.endsWith(extension)) {
-                    filesWithExtension.add(new File(file));
-                }
-            }
-
-            return filesWithExtension;
-
+            afd = context.getAssets().openFd(path + file.getAbsolutePath());
+            mmr.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
+
+        String title =
+                mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+        return title;
+    }
+
+    public String getAudioAlbum(Context context, File file, String path) {
+        AssetFileDescriptor afd;
+        MediaMetadataRetriever mmr = new MediaMetadataRetriever();
+
+        try {
+            afd = context.getAssets().openFd(path + file.getAbsolutePath());
+            mmr.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String album =
+                mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM);
+        return album;
     }
 
     public static MyFunctions getUniqueInstance() {
